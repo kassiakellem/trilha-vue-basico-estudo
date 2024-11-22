50cd2082bb220cc2c45e0bbbd7773df8a413d2a0
kássia kellem <kassiakellem4@gmail.com>
2024-11-22 18:17:49 -0300

    Criação de eventos e métodos.

---
+++ src/components/EventosLifCycleHooks.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue';
+const contador = ref(0)
+const nome = ref ("vue.js")
+const olaVue = ()=> {
+    alert (`Olá ${nome.value}`);
+}
+
+</script>
+
+<template>
+ <button @click="contador++">contador:{{contador}}</button>   
+<button @click="olaVue">Olá Vue </button>
+
+</template>
